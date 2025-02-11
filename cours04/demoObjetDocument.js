d8670a248f2bbd56f2016d80dc465b4538533bcf
shun <shunlegros1@gmail.com>
2025-02-11 10:03:34 -0500

    bruhhjbjgojl

--- cours04/demoObjetDocument.js
+++ cours04/demoObjetDocument.js
@@ -17,17 +17,26 @@ propriétés et méthodes de l'objet document.
 
 console.log("\n1. Accès à l'objet document");
 // Affiche le contenu HTML de l'objet document
-
+console.dir(document);
 console.log("\n2. Accès à des propriétés de l'objet document");
 // Affiche l'URL de la page
+console.log(document.URL);
+console.log(document['URL']);
 // Affiche le titre de la page
+console.log(document.title);
 // Affiche le contenu du corps de la page
+console.log(document.body);
 // Affiche le contenu de l'en-tête de la page
+console.log(document.head);
 // Affiche le type de document
+console.log(document.doctype);
 // Affiche les éléments enfants de la page, dans ce cas, la balise html
-
+console.log(document.children);
 console.log("\n3. Accès à des méthodes de l'objet document");
 // Ouvre un nouvel onglet avec l'URL spécifié (dans la même fenêtre de navigateur)
+// window.open('https://google.ca', '_blank')
 // Ouvre une nouvelle fenêtre et y crée un document
+let nouvelleFenetre = document.open('_blank', '' , 'rel=noopener')
 // Écrit dans le nouveau document ouvert
+nouvelleFenetre.document.write('<p>Bonjour le monde!</p>');
 // Écrit dans le document actuel
--- cours04/demoSelecteurs.js
+++ cours04/demoSelecteurs.js
@@ -3,7 +3,6 @@
 // Sujet: Démo sur les sélecteurs en JavaScript
 
 /*
-
 Références: https://developer.mozilla.org/fr/docs/Web/API/Document/getElementById
             https://developer.mozilla.org/fr/docs/Web/API/Document/getElementsByClassName
             https://developer.mozilla.org/fr/docs/Web/API/Document/getElementsByTagName
@@ -21,11 +20,21 @@ console.log("\n1. Sélecteurs par ID : getElementById");
 
 // Sélectionne l'élément ayant l'ID 'pied'
 // Affiche l'élément sélectionné
+const pied = document.getElementById('pied');
+console.log(pied);
 // Affiche le type et le nom de noeud et le contenu HTML de l'élément sélectionné
-
+console.log('Type de noeud (nodeType)', pied.nodeType);
+console.log('Nom de noeud (nodeName)', pied.nodeName);
+console.log('Contenu HTML (innerHTML)', pied.innerHTML);
+console.log('Contenu HTML (innerHTML)', pied.textContent);
 // Sélectionne l'élément ayant l'ID 'inexistant'
 // Affiche null
-
+const absent = document.getElementById('bruh');
+if (absent ===null){
+    console.log("il n'y a pas d'élément #bruh dans la page.");
+} 
+else { console.log(absent);}
+console.log(absent);
 
 /****************************************************************
 getElementsByClassName('uneClasse') :
@@ -37,19 +46,30 @@ console.log("\n2. Sélecteurs par classe : getElementsByClassName");
 
 // Sélectionne les éléments ayant la classe 'nav-link'
 // Affiche la collection d'éléments sélectionnés
+const navLinks = document.getElementsByClassName('nav-link');
+console.log(navLinks);
 // Affiche le nombre d'éléments sélectionnés
+console.log("il y a" , navLinks.length, "élément ayant la class nav-link");
 // Affiche le premier élément sélectionné,
-                                  // préférable à elmsClasse1[0] pour éviter les erreurs
+console.log(navLinks[0], "est le premier élément"); // préférable à navLinks[0] pour éviter les erreurs
+console.log(navLinks.item[0], "est le premier élément");
 // Parcourir les éléments d'une HTMLCollection avec boucle for... of
 
+for (const elm of navLinks) {
+    console.log(elm.textContent);
+    
+}
+
 // Autre méthode en convertissant la HTMLCollection en tableau avec Array.from()
-
+Array.from(navLinks).forEach(elm => console.log(elm.textContent));
 // Sélectionne les éléments ayant la classe 'inexistant'
-
+const elmBruh = document.getElementsByClassName('inexistant');
+console.log("il y a " , elmBruh.length , "élément avec la classe bruh");
 
 // Sélectionne les éléments ayant les classes 'text-info' et 'text-uppercase'
+const elmUpper = document.getElementsByClassName("text-uppercase text-")
 // l'ordre est sans importance, mais la casse est importante
-
+console.log(elmUpper.item(0));
 // Affiche le premier élément sélectionné
 
 
@@ -61,10 +81,14 @@ getElementsByTagName('uneBalise') :
 console.log("\n3. Sélecteurs par balise : getElementByTagName");
 
 // Sélectionner les éléments <a>
-
+const liens = document.getElementsByTagName('a');
+console.log("text des balise a du document");
+Array.from(liens).forEach(a => console.log(a.textContent));
 // Sélectionne les éléments <head>
+const tete = document.getElementsByTagName('head');
+console.log(tete);
 // Affiche le nombre d'éléments <script>
-
+console.log("il y a " , document.getElementsByTagName('script').length, "balises <script>");
 
 /****************************************************************
 querySelector('selecteurCSS') :
@@ -75,22 +99,30 @@ console.log("\n4. Sélecteur CSS : querySelector");
 
 // Sélectionne le premier élément <p>
 // Affiche l'élément sélectionné
-
+const premierP = document.querySelector('p');
+console.log(premierP);
 // Sélectionne le premier élément <li> du premier <ol>
+const premierLiol = document.querySelector('ol li');
 // Affiche le contenu HTML de l'élément sélectionné
+console.log(premierP.textContent);
 // Donne le même résultat que innerHTML, car le li ne contient pas de balises HTML
 
 
 /****************************************************************
 querySelectorAll('selecteurCSS') :
-    retourne tous les éléments correspondant au sélecteur CSS
+    retourne tous les éléments correspondant au sélecteur CSS sous form de nodeListe
     si des éléments existent, sinon retourne une collection vide
 ****************************************************************/
 console.log("\n5. Sélecteur CSS : querySelectorAll");
 
 // Sélectionne tous les éléments <p>
+const tousLesP = document.querySelectorAll('p');
+console.log(tousLesP);
 // Parcourir les éléments d'une NodeList
 // Affiche le texte de chaque élément sélectionné
-
+tousLesP.forEach(p => console.log(p.textContent));
 // Sélectionne tous les éléments <li> de tous les <ul>
+const tousUlli = document.querySelectorAll('ul li');
+console.log(tousUlli);
 // Affiche le nombre d'éléments <li> sélectionnés
+console.log("il y a", tousUlli.length, "<li> dans des <ul>");
